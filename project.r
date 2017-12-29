9db6df5fc0188a294bf96e954a7f2b4d4d8b70e7
Marc <marcvila10@hotmail.com>
2017-12-29 17:42:41 +0100

    First test naive bayes

--- project.r
+++ project.r
@@ -8,65 +8,67 @@ set.seed (6046)
 
 ## Getting the dataset
 
-deposit <- read.table ("Bank Marketing DATA/bank-full.csv", header=TRUE, stringsAsFactors=TRUE, sep=";")
+deposit <- read.table ("dataset/bank-full.csv", header=TRUE, stringsAsFactors=TRUE, sep=";")
 
 dim(deposit)
-
 summary(deposit)
 
 ## This dataset needs a lot of pre-processing ... also it displays a good mixture of categorical and numeric variables: LDA/QDA may not the best choice (better use LogReg)
 
-# age seems OK
-# job has 12 values, let's check their frequency
+# 'age' has 77 values, seems OK, age and number of people with each 'age'
+table(deposit$age)
 
-# seems OK
+# 'job' has 12 values, seems OK
+# 288 UNKNOWN
 table(deposit$job)
 
-# education has 4 values, let's check their frequency
-
-# seems OK
+# 'education' has 4 values, seems OK
+# 4 UNKNOWN
 table(deposit$education)
 
-# month looks very suspicious ... but is OK
-
+# 'month' has 12 values, looks very suspicious ... but is OK
 table(deposit$month)
 
-# highly skewed ...
-
+# 'duration' has more than 500 values, highly skewed ...
 hist(deposit$duration)
 hist(log(deposit$duration))
 
 deposit$duration <- log(deposit$duration+0.001)
 
-# what to do with 'pdays' and 'previous'? it is not clear ... we leave as it is
+# 'pdays' and 'previous'? it is not clear... we leave as it is
 
 ## the 'unknown' and "-1" may need some traatment
 
 ## The rest seem OK (it would take a careful analysis, and a lot of domain knowledge)
 
-# I rename the target ...
+# Rename the target value from 'y' to 'subscribed'
 
 colnames(deposit)[ncol(deposit)] <- "subscribed"
 
 dim(deposit)
-
 summary(deposit)
 
-## Let's have a visual inspection of the continuous variables
+## Let's have a visual inspection 
 
 library(reshape2)
 library(ggplot2)
+
+## Visual inspection of the continuous variables
 
 d.cont <- melt(deposit[,c("age","balance","duration","campaign","pdays","previous")])
 ggplot(d.cont,aes(x = value)) + 
   facet_wrap(~variable,scales = "free") + geom_histogram()
 
-## Let's have a visual inspection of the factor variables
+## Visual inspection of the factor variables
 
 d.categ <- melt(deposit, measure.vars=c("job","marital","education","housing","loan","contact","default", "poutcome"))
 ggplot(d.categ,aes(x = value)) + facet_wrap(~variable,scales = "free") + geom_bar()
 
-## Preparing dataset for modeling: we should consider taking log10 of other variables, scaling the continuous ones (after the eventual log10s), treat the '999' and 'unknown', balance the errors (if need be) ...
+## Preparing dataset for modeling, we should consider:
+# taking log10 of other variables,
+# scaling the continuous ones (after the eventual log10s),
+# treat the '999' and 'unknown', 
+# balance the errors (if need be) ...
 
 ## Since we want to use different methods, we need CV and a separate test set:
 
@@ -82,3 +84,36 @@ nlearn <- length(learn.indexes)
 ntest <- N - nlearn
 
 ...
+
+### Marc:
+
+# Naive Bayes Classifier
+
+library (e1071)
+
+depositBayes <- deposit[c("job", "marital")]
+
+colnames(depositBayes) <- c(" job", "marital")
+namevector <- c("Class")
+
+depositBayes[ , namevector] <- 0
+
+summary(depositBayes)
+
+set.seed(1111)
+
+N <- nrow(depositBayes)
+
+## We first split the available data into learning and test sets, selecting randomly 2/3 and 1/3 of the data
+## We do this for a honest estimation of prediction performance
+
+learn <- sample(1:N, round(2*N/3))
+nlearn <- length(learn)
+ntest <- N - nlearn
+
+model <- naiveBayes(Class ~ ., data = depositBayes[learn,])
+model
+
+pred <- predict(model, depositBayes[1:20, -1], type="raw")
+
+table(pred, depositBayes$job)
